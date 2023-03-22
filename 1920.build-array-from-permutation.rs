002135fecdc2194cc4537430a90d1d1fb948724d
eshad <fidaeshad10@gmail.com>
2023-03-22 17:21:39 -0500

    leetcode rust via vs code

---
+++ 1920.build-array-from-permutation.rs
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=1920 lang=rust
+ *
+ * [1920] Build Array from Permutation
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut ans = vec![0; nums.len()];
+
+        for i in (0..nums.len()){ String::from("dasf")
+            ans[i] = nums[nums[i] as usize];
+        }
+        ans
+    }
+}
+// @lc code=end
+
---
+++ 1925.count-square-sum-triples.rs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1925 lang=rust
+ *
+ * [1925] Count Square Sum Triples
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn count_triples(n: i32) -> i32 {
+        
+    }
+}
+// @lc code=end
+
